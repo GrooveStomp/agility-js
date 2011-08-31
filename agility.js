e427dd1fcd6da7400ab633f8d798d451214edd92
GrooveStomp <groovestomp@gmail.com>
2011-08-30 23:29:35 -0700

    Update main.js to work with updated agility.js, illustrating JSON=>HTML and JSON=>CSS DSLs.

---
+++ agility.js
@@ -0,0 +1,1 @@
+../agility-groovestomp.git/agility.js
--- main.js
+++ main.js
@@ -1,44 +1,45 @@
-var error = $$({
-    model: {msg:''},
-    view: {
-        format: ' \
-            <div> \
-                <span data-bind="msg"/> \
-                <span id="close">(Click anywhere to close)</span> \
-            </div>',
-        style: GrooveStomp.css({
-            "&": {
-                "position": "fixed",
-                "top": 0,
-                "left": 0,
-                "right": 0,
-                "z-index": 1000,
-                "background": "#f55",
-                "color": "white",
-                "display": "none",
-                "padding": "5px 15px"
-            },
-            "& #close": {
-                "float": "right"
-            }
-        })
-    },
-    controller: {},
-
-    // User-defined
-    show: function(s){
-        var self = this;
-        this.model.set({ "msg": s });
-        this.view.$().slideDown(200, function() {
-            $(document).one('click', function() {
-                self.hide();
-            });
-        });
-    },
-    hide: function() {
-        this.view.$().slideUp(100);
+var error = agility({
+  model: { "msg": '' },
+  view: {
+    format: [
+      "div", [
+        ["span", {"data-bind": "msg"}],
+        ["span", {"id": "close"}, "(Click anywhere to close)"]
+      ]
+    ],
+    style: {
+      "&": {
+        "position": "fixed",
+        "top": 0,
+        "left": 0,
+        "right": 0,
+        "z-index": 1000,
+        "background": "#f55",
+        "color": "white",
+        "display": "none",
+        "padding": "5px 15px"
+      },
+      "& #close": {
+        "float": "right"
+      }
     }
+  },
+  controller: {},
+
+  // User-defined
+  show: function(s){
+    var self = this;
+    this.model.set({ "msg": s });
+    this.view.$().slideDown(200, function() {
+      $(document).one('click', function() {
+        self.hide();
+      });
+    });
+  },
+  hide: function() {
+    this.view.$().slideUp(100);
+  }
 });
 
-$$.document.append(error);
+agility.document.append(error);
 error.show("Hello");
